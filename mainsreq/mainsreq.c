ad76108948ad2f0857e8ad814f9adfe2e477e105
Family Karamatov <familykaramatov@pc-4-3.customer.ask4.lan>
2023-03-10 00:07:29 +0000

    syntax fix

--- mainsreq/mainsreq.c
+++ mainsreq/mainsreq.c
@@ -2,14 +2,14 @@
 //PWM-----------------------------------------------------
 void init_PWM(void)
 {
-	DDR5 |= _BV(PD5);
+	DDRD |= _BV(PD5);
 	TCCR1A = _BV(COM1A0)
 		 |_BV(WGM10);
 	TCCR1B = _BV(WGM13)
 		 | _BV(CS11);
 }
 
-void PWM(uint16_t FREQ, uint16_t duty_cycle)
+void PWM(uint16_t frequency, uint16_t duty_cycle)
 {
     OC1RA = (uint16_t) duty_cycle;
 }
