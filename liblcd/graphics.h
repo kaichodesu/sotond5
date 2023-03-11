6bd8560fc7f21e48e1404fd5241f08cfea804d8c
mbt0ckaicho <mbt0ckaicho@protonmail.ch>
2023-03-11 10:58:20 +0000

    font and graphics update

--- liblcd/graphics.h
+++ liblcd/graphics.h
@@ -2,4 +2,8 @@
 
 extern const uint16_t bg1_1[] PROGMEM;
 extern const uint16_t bg1_2[] PROGMEM;
-extern const uint16_t bg1_3[] PROGMEM;
+extern const uint16_t digit3[] PROGMEM;
+extern const uint16_t digit2[] PROGMEM;
+extern const uint16_t digit1[] PROGMEM;
+extern const uint16_t BATCHR[] PROGMEM;
+extern const uint16_t BATDRN[] PROGMEM;
